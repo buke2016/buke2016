e05b8269e6b2e923ef8872a52263d58dc07599fa
Peter <33225148+buke2016@users.noreply.github.com>
2023-09-12 12:49:08 -0700

    Helloworld.c

--- cmsi2120/classwork/classwork04/Search for a rangeleetcode.py
+++ cmsi2120/classwork/classwork04/Search for a rangeleetcode.py
@@ -25,3 +25,19 @@ class Solution:
             return [left, right]
         else:
             return [-1, -1]
+        
+nums1 = [5, 7, 7, 8, 8, 10]
+target1 = 8
+result1 = search_range(nums1, target1)
+print(result1)  # Output: [3, 4]
+
+nums2 = [5, 7, 7, 8, 8, 10]
+target2 = 6
+result2 = search_range(nums2, target2)
+print(result2)  # Output: [-1, -1]
+
+nums3 = []
+target3 = 0
+result3 = search_range(nums3, target3)
+print(result3)  # Output: [-1, -1]
+
